5863cc83611afb4ae4ad2a036ecf77a329c5507b
Anhelina Vyshniak <avyshniak@kse.org.ua>
2024-04-17 21:34:21 +0300

    views.sql

--- views.sql
+++ views.sql
@@ -2,26 +2,26 @@
 -- It displays the title, comma-separated authors, publisher name, release year, loan date in a formatted way,
 -- and the number of days until the end of the term.
 CREATE VIEW get_rented_books_view AS
-SELECT
-    b.title AS title,
-    GROUP_CONCAT(a.name_ ORDER BY a.name_ ASC SEPARATOR ', ') AS authors,
-    CONCAT(p.name_, ', ', b.year_publishing) AS publisher_and_year,
-    MAX(DATE_FORMAT(l.loan_date, '%b, %d %Y')) AS loan_date,
-    MAX(DATEDIFF(l.due_date, CURDATE())) AS days_until_end_term
-FROM
-    book b
-JOIN
-    book_author ba ON b.id = ba.book_id
-JOIN
-    author a ON ba.author_id = a.id
-JOIN
-    loan l ON b.id = l.book_id
-JOIN
-    publisher p ON b.publisher_id = p.id
-WHERE
-    l.return_date IS NULL
-GROUP BY
-    b.id, b.title, p.name_, b.year_publishing;
+    SELECT
+        b.title AS title,
+        GROUP_CONCAT(a.name_ ORDER BY a.name_ ASC SEPARATOR ', ') AS authors,
+        CONCAT(p.name_, ', ', b.year_publishing) AS publisher_and_year,
+        MAX(DATE_FORMAT(l.loan_date, '%b, %d %Y')) AS loan_date,
+        MAX(DATEDIFF(l.due_date, CURDATE())) AS days_until_end_term
+    FROM
+        book b
+    INNER JOIN
+        book_author ba ON b.id = ba.book_id
+    INNER JOIN
+        author a ON ba.author_id = a.id
+    INNER JOIN
+        loan l ON b.id = l.book_id
+    INNER JOIN
+        publisher p ON b.publisher_id = p.id
+    WHERE
+        l.return_date IS NULL
+    GROUP BY
+        b.id, b.title, p.name_, b.year_publishing;
 
 -- Indexes are implemented on pertinent columns to enhance search efficiency,
 -- including `title` in the `book` table, `name_` in the `author` table, and `return_date` in the `loan` table.
